5daeeed73731d95494407c1330195333d67e1c98
Daniel-Lucas-Caldas <143418936+Daniel-Lucas-Caldas@users.noreply.github.com>
2023-12-06 08:29:28 -0300

    Aula 08 - 0412 - Pares e Impars - função.c

--- Aula 08 - 0412 - Pares e Impars - função.c
+++ Aula 08 - 0412 - Pares e Impars - função.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <locale.h>
+#include <string.h>
+
+#define Quanti 6
+
 
 void cabecalho () {
 	printf("\n=====================================\n");
@@ -8,34 +12,46 @@ void cabecalho () {
     printf("\n=====================================\n");
 }
 
-void verificarQuantidadeParOuImpar (int numero[]) {
-    int i, pares = 0, impares = 0;
-  
-  for (i = 0; i < 6; i++) {
+void verificarQuantidadePar (int numero[]) {
+     int i, pares = 0;
+
+  for (i = 0; i < Quanti; i++) {
       if (numero[i] % 2 == 0) {
             pares++;
-        } else {
+        }
+  }
+
+   printf("\nQuantidade de números pares: %d \n", pares);
+}
+
+void verificarQuantidadeImpar (int numero[]) {
+    int i, impares = 0;
+
+  for (i = 0; i < Quanti; i++) {
+      if (numero[i] % 2 != 0) {
             impares++;
         }    
   }
 
-   printf("\nQuantidade de números pares: %d \n", pares);
    printf("\nQuantidade de números impares: %d \n", impares);
 }
 
 int main () {
     setlocale(LC_ALL, "portuguese");
-    int numero[6];
+    int numero[Quanti];
     int i;
 
     cabecalho ();
-    for (i = 0; i < 6; i++) {
+    for (i = 0; i < Quanti; i++) {
         printf("Digite um número qualquer: ");
         scanf("%d", &numero[i]);
     }
     
+    system("cls || clear");
+
     cabecalho ();
-    verificarQuantidadeParOuImpar(numero);
+    verificarQuantidadePar(numero);
+    verificarQuantidadeImpar(numero);
 
   return 0;
 } 
